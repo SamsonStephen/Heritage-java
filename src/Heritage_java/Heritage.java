8c669edc6bdf9f5fe1c2b947bfe44b4ffb81b20f
Samson Stephen <80698808+SamsonStephen@users.noreply.github.com>
2022-01-31 16:40:07 +0100

    Update Heritage.java

--- src/Heritage_java/Heritage.java
+++ src/Heritage_java/Heritage.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Heritage_java;
 import java.util.ArrayList;
 /**
- *
- * @author test
+ * Utilisation de la fonction extends et super()
+ * 
  */
 public class Heritage {
     
